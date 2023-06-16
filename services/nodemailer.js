f85d62f97fa66bad0c906cc6b0b896b7b0340086
remystreich <remystreich@gmail.com>
2023-06-16 18:40:47 +0200

    nodemailer

--- services/nodemailer.js
+++ services/nodemailer.js
@@ -1,12 +1,12 @@
 const nodemailer = require('nodemailer');
-
+require('dotenv').config()
 
 const transporter = nodemailer.createTransport({  
     host: 'smtp.gmail.com',
     port: 587, // 587 -> TLS & 465 -> SSL
     auth: {  
-      user: process.env.USERMAIL, // email de votre votre compte google
-      pass: process.env.PASSMAIL// password de votre compte google
+      user: process.env.USERMAIL,
+      pass: process.env.PASSMAIL,
     }  
   });
 
